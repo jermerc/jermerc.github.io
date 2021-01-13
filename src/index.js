ed11015f57a18e2b444ad852ab716d46c0ba45e9
root <jermaine.mercado7@gmail.com>
2021-01-12 23:05:24 -0500

    Trying to fix styling issues.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/line-awesome/dist/line-awesome/css/line-awesome.css';
 
 ReactDOM.render(
   <React.StrictMode>
