61aa8ea57fd149bc3d783f830a5e3b7dbba2394f
root <jermaine.mercado7@gmail.com>
2021-05-26 17:57:01 -0400

    Updated resume.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,7 +8,15 @@ class AboutMe extends React.Component {
         return (
             <div className="four columns u-pull-left">
                 <h1>Jermaine Mercado</h1>
-                <h6>Software Engineering @ University of Guelph 2023</h6>
+                <h5>Software Engineering @ University of Guelph 2023</h5>
+
+                <h6>Contact Me!</h6>
+                <p>
+                    <i style={{color: "white"}} class="las la-envelope"></i><a href="mailto:jermaine.mercado7@gmail.com">Email</a>
+                    <i style={{color: "white"}} class="lar la-file"></i><a href="https://drive.google.com/file/d/1Gra014qMSC-YHad6sStr3nfPZJFHw0XV/view">Resume</a>
+                </p>
+
+
                 <p>
                     <hr/>
                     <i class="las la-code"></i>Languages: JavaScript, Python, Java, C/C++<br />
